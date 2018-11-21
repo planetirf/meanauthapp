132bf0d37daf70b263a18c0e803c9b77bd03298b
Irfan Ainuddin <irfandin@gmail.com>
2018-11-21 13:54:38 -0800

    linked mlab remote db

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    // database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb://planetirf:H@@ploxer011@ds119650.mlab.com:19650/meanauthapp'
     secret: 'irfsecret'
 }
