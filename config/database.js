adbebcbcb7cb2fa227151327d918ba34adf52308
Irfan Ainuddin <irfandin@gmail.com>
2018-11-21 14:03:34 -0800

    mlab link borked fix2

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    database: 'mongodb://planetirf:vandals1@ds119650.mlab.com:19650/meanauthapp'
+    database: 'mongodb://planetirf:vandals1@ds119650.mlab.com:19650/meanauthapp',
     secret: 'irfsecret'
 }
