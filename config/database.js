cc9a490b18c701d336a9996f0d6424082ed01c85
Irfan Ainuddin <irfandin@gmail.com>
2018-11-21 13:56:03 -0800

    removed linked mlab remote db2

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-    // database: 'mongodb://localhost:27017/meanauth',
-    database: 'mongodb://planetirf:H@@ploxer011@ds119650.mlab.com:19650/meanauthapp'
+    database: 'mongodb://localhost:27017/meanauth',
     secret: 'irfsecret'
 }
